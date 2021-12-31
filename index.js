23639154e56a2492f87aa089bfc190ac942d66b0
Ard-Jan van Etten <ardjanve@gmail.com>
2021-12-31 13:22:30 +0100

    update multiplanner

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const formatteerReis = require('./functies/formatteerReis.js');
 
 const {
-    multiReis
+    multiReis,
+    planReis,
 } = require("multiplanner");
 
 (async () => {
@@ -13,7 +14,9 @@ const {
     if (!file) return console.log("Geef een bestand op met de reis");
     const rawRoute = await fs.promises.readFile(path.join(process.cwd(), process.argv[2]));
     if (!rawRoute) return console.log("Bestand niet gevonden.");
-    const reis = await multiReis(rawRoute.toString());
+    const route = rawRoute.toString();
+    const reisJSON = multiReis(route);
+    const reis = await planReis(reisJSON);
     const reisScriptNederlands = formatteerReis(reis);
     console.log(reisScriptNederlands);
 })();
